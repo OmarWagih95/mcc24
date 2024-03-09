ea5b3f805d1688c663a3c8704e55989506248fd5
omarwagih <omarwagih95@gmail.com>
2024-03-10 00:23:46 +0200

    firebase configured

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,20 @@ import 'package:androidstudiommc/cubits/LanguagesCupitStates.dart';
 import 'package:androidstudiommc/generated/l10n.dart';
 import 'package:androidstudiommc/routing/app_router.dart';
 import 'package:androidstudiommc/routing/routes.dart';
-import 'package:androidstudiommc/views/selectLanguage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:intl/intl.dart';
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+
+import 'firebase_options.dart';
+
+void main() async{
+    WidgetsFlutterBinding.ensureInitialized();
+
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+
   runApp(BlocProvider(
     create: (context) => LanguagesCubit(),
     child: MyApp(
