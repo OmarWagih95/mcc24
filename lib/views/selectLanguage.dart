4135f3ac332da6a72d77cbb0e3e66ec0dda4c849
amr <amr.soubhi.hassanieen@gmail.com>
2024-03-16 16:08:17 +0200

    add localization for select language view

--- lib/views/selectLanguage.dart
+++ lib/views/selectLanguage.dart
@@ -1,79 +1,81 @@
 import 'package:MCC/cubits/LanguagesCupit.dart';
+import 'package:MCC/generated/l10n.dart';
 import 'package:MCC/routing/routes.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-class chooseLanguages extends StatefulWidget {
+class chooseLanguages extends StatelessWidget {
   bool onBoard;
   chooseLanguages(this.onBoard);
 
   @override
-  State<chooseLanguages> createState() => _chooseLanguagesState();
-}
-
-class _chooseLanguagesState extends State<chooseLanguages> {
-  bool isHovered = false;
-
-  @override
   Widget build(BuildContext context) {
-    print(widget.onBoard);
+    print(onBoard);
     return SafeArea(
       child: Scaffold(
         backgroundColor: Colors.grey[50],
         body: Padding(
           padding: const EdgeInsets.all(20),
           child: SizedBox(
-                    height: 150.h,
-                    child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Text(
-              'language',
-              style: TextStyle(fontWeight: FontWeight.w500, fontSize: 24),
-            ),
-            Text(
-              ' choose language of the interface',
-              style: TextStyle(fontSize: 18.sp),
-            ),
-            Row(
+            height: 150.h,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                ElevatedButton(
-                  style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all(Colors.white),
-                      minimumSize: MaterialStateProperty.all(Size(180.w, 40.h))),
-                  onPressed: () {
-                    BlocProvider.of<LanguagesCubit>(context).changeLanguages(false);
-                  widget.onBoard?  Navigator.pushNamed(context, '${Routes.onboargingScreen}'):
-                  Navigator.pop(context);
-                  },
-                  child: const Text(
-                    'العربية',
-                    style: TextStyle(color: Colors.blueGrey),
-                  ),
+                Text(
+                  S.of(context).language,
+                  style: TextStyle(fontWeight: FontWeight.w500, fontSize: 24),
                 ),
-                ElevatedButton(
-                    onPressed: () {
-                      BlocProvider.of<LanguagesCubit>(context)
-                          .changeLanguages(true);
-                      widget.onBoard?  Navigator.pushNamed(context, '${Routes.onboargingScreen}'):
-                      Navigator.pop(context);
-                    },
-                    style: ButtonStyle(
-                        minimumSize: MaterialStateProperty.all(Size(180, 40)),
-                        backgroundColor:
-                            MaterialStateProperty.all(Colors.white)),
-                    child: const Text(
-                      'ُEnglish',
-                      style: TextStyle(color: Colors.blueGrey),
-                    )),
-              ],
-            )
-          ],
+                Text(
+                  S.of(context).Choose_Language_Of_The_Interface,
+                  style: TextStyle(fontSize: 18.sp),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    ElevatedButton(
+                      style: ButtonStyle(
+                          backgroundColor:
+                              MaterialStateProperty.all(Colors.white),
+                          minimumSize:
+                              MaterialStateProperty.all(Size(180.w, 40.h))),
+                      onPressed: () {
+                        BlocProvider.of<LanguagesCubit>(context)
+                            .changeLanguages(false);
+                        onBoard
+                            ? Navigator.pushNamed(
+                                context, Routes.onboargingScreen)
+                            : Navigator.pop(context);
+                      },
+                      child: Text(
+                        S.of(context).Arabic,
+                        style: TextStyle(color: Colors.blueGrey),
+                      ),
                     ),
-                  ),
+                    ElevatedButton(
+                        onPressed: () {
+                          BlocProvider.of<LanguagesCubit>(context)
+                              .changeLanguages(true);
+                          onBoard
+                              ? Navigator.pushNamed(
+                                  context, Routes.onboargingScreen)
+                              : Navigator.pop(context);
+                        },
+                        style: ButtonStyle(
+                            minimumSize:
+                                MaterialStateProperty.all(Size(180.w, 40.h)),
+                            backgroundColor:
+                                MaterialStateProperty.all(Colors.white)),
+                        child: Text(
+                          S.of(context).English,
+                          style: const TextStyle(color: Colors.blueGrey),
+                        )),
+                  ],
+                )
+              ],
+            ),
+          ),
         ),
       ),
     );
