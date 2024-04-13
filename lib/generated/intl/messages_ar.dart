72bd7b79c0518036536a76a4cfe221d054130298
amr <amr.soubhi.hassanieen@gmail.com>
2024-04-13 18:06:01 +0200

    ...

---
+++ lib/generated/intl/messages_ar.dart
@@ -0,0 +1,84 @@
+// DO NOT EDIT. This is code generated via package:intl/generate_localized.dart
+// This is a library that provides messages for a ar locale. All the
+// messages from the main program should be duplicated here with the same
+// function name.
+
+// Ignore issues from commonly used lints in this file.
+// ignore_for_file:unnecessary_brace_in_string_interps, unnecessary_new
+// ignore_for_file:prefer_single_quotes,comment_references, directives_ordering
+// ignore_for_file:annotate_overrides,prefer_generic_function_type_aliases
+// ignore_for_file:unused_import, file_names, avoid_escaping_inner_quotes
+// ignore_for_file:unnecessary_string_interpolations, unnecessary_string_escapes
+
+import 'package:intl/intl.dart';
+import 'package:intl/message_lookup_by_library.dart';
+
+final messages = new MessageLookup();
+
+typedef String MessageIfAbsent(String messageStr, List<dynamic> args);
+
+class MessageLookup extends MessageLookupByLibrary {
+  String get localeName => 'ar';
+
+  final messages = _notInlinedMessages(_notInlinedMessages);
+  static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
+        "Adress": MessageLookupByLibrary.simpleMessage("العنوان"),
+        "Arabic": MessageLookupByLibrary.simpleMessage("العربية"),
+        "Choose_Language_Of_The_Interface":
+            MessageLookupByLibrary.simpleMessage("اختار لغة واجهة التطبيق "),
+        "Choose_your_service_easily":
+            MessageLookupByLibrary.simpleMessage("اختار الخدمة بسهولة"),
+        "Complete_the_service_and_rate_the_service_provider":
+            MessageLookupByLibrary.simpleMessage(
+                "انهي الخدمة وقيم مزود الخدمة"),
+        "Continue": MessageLookupByLibrary.simpleMessage("استمرار"),
+        "Describe_your_request":
+            MessageLookupByLibrary.simpleMessage("اكتب وصفا لطلبك"),
+        "Dont_have_an_account_yet":
+            MessageLookupByLibrary.simpleMessage("ليس لديك حساب بعد ؟ "),
+        "English": MessageLookupByLibrary.simpleMessage("الأنجليزية"),
+        "Forgot_Password":
+            MessageLookupByLibrary.simpleMessage(" هل نسيت الباسورد ؟"),
+        "General_contracting":
+            MessageLookupByLibrary.simpleMessage("مقاولات عامة"),
+        "General_interior_design_and_finishes":
+            MessageLookupByLibrary.simpleMessage("ديكورات وتشطيبات عامة"),
+        "General_maintenance_and_renovation":
+            MessageLookupByLibrary.simpleMessage("صيانة عامة وتجديد"),
+        "Home": MessageLookupByLibrary.simpleMessage("الرئيسية"),
+        "Language_Exchange":
+            MessageLookupByLibrary.simpleMessage(" تغيير اللغة"),
+        "Login": MessageLookupByLibrary.simpleMessage("دخول"),
+        "Maintainance_Packages":
+            MessageLookupByLibrary.simpleMessage("باقات الصيانة"),
+        "My_Order": MessageLookupByLibrary.simpleMessage("طلبي"),
+        "Phone_number": MessageLookupByLibrary.simpleMessage("رقم الهاتف"),
+        "Remember_me": MessageLookupByLibrary.simpleMessage(" تذكرني"),
+        "Search_for_service_or_product":
+            MessageLookupByLibrary.simpleMessage("ابحث عن خدمة او منتج"),
+        "Select_the_best_offer_for_you": MessageLookupByLibrary.simpleMessage(
+            "اختر العرض الأفضل بالنسبة لك"),
+        "Settings": MessageLookupByLibrary.simpleMessage("الأعدادات"),
+        "Share_Application":
+            MessageLookupByLibrary.simpleMessage("مشاركة التطبيق"),
+        "Sign_IN": MessageLookupByLibrary.simpleMessage("تسجيل الدخول"),
+        "Sign_Up": MessageLookupByLibrary.simpleMessage("قم بأنشاء حساب "),
+        "Text_Us": MessageLookupByLibrary.simpleMessage("راسلنا"),
+        "Text_us_for_any_help_or_question":
+            MessageLookupByLibrary.simpleMessage(
+                "دردش معنا مباشرة لأي مساعدة او استفسار"),
+        "We_are_excited_to_have_you_back": MessageLookupByLibrary.simpleMessage(
+            "متحمسين بعودتك لا يمكنا الانتظار لمعرفة اخر نشاطك منذ اخر تسجيل دخول لك"),
+        "Welcome_Back": MessageLookupByLibrary.simpleMessage("مرحبا بعودتك"),
+        "Who_Are": MessageLookupByLibrary.simpleMessage("من نحن"),
+        "choose_maintenance_service":
+            MessageLookupByLibrary.simpleMessage("اختار خدمة الصيانة"),
+        "email": MessageLookupByLibrary.simpleMessage("الإيميل"),
+        "language": MessageLookupByLibrary.simpleMessage("اللغة"),
+        "password": MessageLookupByLibrary.simpleMessage("الباسورد "),
+        "skip": MessageLookupByLibrary.simpleMessage("تخطي"),
+        "then_get_best_prices_from_our_suppliers":
+            MessageLookupByLibrary.simpleMessage(
+                "ثم احصل علي افضل عروض الأسعار من مزودينا")
+      };
+}
